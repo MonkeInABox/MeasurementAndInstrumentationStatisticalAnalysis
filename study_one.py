05bad9555c011b6f87549ef9a5c544791e959f6b
Jeremy <jeremynbutson@gmail.com>
2026-07-29 11:55:52 +0800

    terwfe

--- study_one.py
+++ study_one.py
@@ -1,8 +1,33 @@
-import numpy
-import matplotlib
+import numpy as np
+import matplotlib as plot
+
+# PART A
+"""
+Generate 10^5 values to create dataset for OPA445AP
+Get mean, std, max, min
+Create histogram
+"""
+def data():
+    dataset = np.random.uniform(0, 3, 100000)
+    print("mean = " + str(np.mean(dataset)))
+    print("std = " + str(np.std(dataset)))
+    print("max = " + str(np.max(dataset)))
+    print("min = " + str(np.min(dataset)))
+    return 0
+
+# PART B
+"""
+Extract samples to specify in datasheet that we have OA with a typical value of 1mV and a max of 3mV
+Statistics and number with this higher performance
+Statistics and number of other
+Histograms
+"""
+def sort():
+    return 0
 
 def main():
-    sys.exit(0)
+    data()
+    sort()
 
 if __name__ == "__main__":
     main()
